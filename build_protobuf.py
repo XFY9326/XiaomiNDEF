1d6fc0a861d132808181b8c9a6ccea1297e31e92
XFY9326 <xfy9326@qq.com>
2024-02-26 12:49:44 +0800

    Fix

--- build_protobuf.py
+++ build_protobuf.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
 
-PROJECT_ROOT = Path(__file__).parent.absolute()
+PROJECT_ROOT = Path(__file__).resolve().parent.absolute()
 
 PROTOC_PATH = "protoc"
 PROTO_DIR = PROJECT_ROOT.joinpath("proto")
@@ -12,6 +12,7 @@ if __name__ == "__main__":
         print("protoc not found")
         exit(1)
 
+    print()
     if PYTHON_OUTPUT_DIR.exists():
         old_files = [i for i in os.listdir(PYTHON_OUTPUT_DIR) if i.endswith(("_pb2.py", "_pb2.pyi"))]
         if len(old_files) > 0:
@@ -23,11 +24,21 @@ if __name__ == "__main__":
         PYTHON_OUTPUT_DIR.mkdir()
         PYTHON_OUTPUT_DIR.joinpath("__init__.py").touch()
 
+    print()
     print("Creating protobuf files")
-    os.system(" ".join([
+    proto_files = [f"\"{i}\"" for i in PROTO_DIR.glob("*.proto")]
+    protoc_cmd = [
         str(PROTOC_PATH),
         f"--proto_path=\"{PROTO_DIR}\"",
         f"--pyi_out=\"{PYTHON_OUTPUT_DIR}\"",
         f"--python_out=\"{PYTHON_OUTPUT_DIR}\"",
-        f"\"{PROTO_DIR.joinpath('*')}\""
-    ]))
+        *proto_files
+    ]
+    print("Commands:", "\n".join(protoc_cmd))
+
+    result = os.system(" ".join(protoc_cmd))
+    print()
+    if result == 0:
+        print("Protobuf files create success")
+    else:
+        print("Protobuf files create failed")
